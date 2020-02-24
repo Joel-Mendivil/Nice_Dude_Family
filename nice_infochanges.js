57bc921d8aaf6ad77eccd25cb96e6520922f2136
Joel Mendivil <jmendivil171@west-mec.org>
2020-02-24 15:27:59 -0700

    Finished with content and added javascript to change themes

---
+++ nice_infochanges.js
@@ -0,0 +1,15 @@
+function defaultTheme(){
+    document.getElementsByTagName("link")[0].setAttribute("href","nice_infostyles.css");
+}
+
+function slickTheme(){
+    document.getElementsByTagName("link")[0].setAttribute("href","nice_infostyles(slick).css");
+}
+
+function westmecTheme(){
+    document.getElementsByTagName("link")[0].setAttribute("href","nice_infostyles(westmec).css");
+}
+
+function bawTheme(){
+    document.getElementsByTagName("link")[0].setAttribute("href","nice_infostyles(baw).css");
+}
