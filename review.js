bc9ba6b41737816c1c0dd3e66563e9c5ac8d5a33
audreysparkss <asparks187@west-mec.org>
2020-02-25 15:27:27 -0700

    added java and css buttons to review and about us
    complete

---
+++ review.js
@@ -0,0 +1,15 @@
+function defaultTheme(){
+    document.getElementsByTagName("link")[0].setAttribute("href","review.css");
+}
+
+function sunsetTheme(){
+    document.getElementsByTagName("link")[0].setAttribute("href","sunset.css");
+}
+
+function cloverTheme(){
+    document.getElementsByTagName("link")[0].setAttribute("href","clover.css");
+}
+
+function rainbowTheme(){
+    document.getElementsByTagName("link")[0].setAttribute("href","rainbow.css");
+}
