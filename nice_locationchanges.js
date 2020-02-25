5ae27e4a59d165cc95535505509c0f465e2a7a56
Joel Mendivil <jmendivil171@west-mec.org>
2020-02-25 15:25:19 -0700

    finished html and themes

--- nice_locationchanges.js
+++ nice_locationchanges.js
@@ -1,15 +1,19 @@
 function defaultTheme(){
     document.getElementsByTagName("link")[0].setAttribute("href","nice_location.css");
+    document.getElementById("logo").setAttribute("src", "logo design prince.jpg");
 }
 
 function toxicTheme(){
     document.getElementsByTagName("link")[0].setAttribute("href","nice_location(luxe).css");
+    document.getElementById("logo").setAttribute("src", "logo design luxe.jpg");
 }
 
 function stormTheme(){
     document.getElementsByTagName("link")[0].setAttribute("href","nice_location(storm).css");
+    document.getElementById("logo").setAttribute("src", "logo design storm.jpg");
 }
 
 function vendettaTheme(){
     document.getElementsByTagName("link")[0].setAttribute("href","nice_location(vendetta).css");
+    document.getElementById("logo").setAttribute("src", "logo design vendetta.jpg");
 }
