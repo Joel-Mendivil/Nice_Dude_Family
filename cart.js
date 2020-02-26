d41d69a09aa5be2ed15e0990044d083e052daeb0
Joel Mendivil <jmendivil171@west-mec.org>
2020-02-26 13:56:36 -0700

    Finished cart page and themes

---
+++ cart.js
@@ -0,0 +1,19 @@
+function defaultTheme(){
+    document.getElementsByTagName("link")[0].setAttribute("href","cart.css");
+    document.getElementById("logo").setAttribute("src", "logo design .jpg");
+}
+
+function iris(){
+    document.getElementsByTagName("link")[0].setAttribute("href","cart(iris).css");
+    document.getElementById("logo").setAttribute("src", "logo design iris.jpg");
+}
+
+function brazil(){
+    document.getElementsByTagName("link")[0].setAttribute("href","cart(brazil).css");
+    document.getElementById("logo").setAttribute("src", "logo design brazil.jpg");
+}
+
+function italy(){
+    document.getElementsByTagName("link")[0].setAttribute("href","cart(italy).css");
+    document.getElementById("logo").setAttribute("src", "logo design italy.jpg");
+}
